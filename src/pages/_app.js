35abe01ca553c7460cc0501d61149a0dd3ec66d5
Jiayin <jl13153@nyu.edu>
2024-12-06 22:10:26 +0800

    try4

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import "/Users/cathyluo/Desktop/nyu /Fall 2024/info visualization/info project/info_project/src/styles/globals.css";
+import "../../src/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
